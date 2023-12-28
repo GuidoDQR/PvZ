42d191dbe9f31482d8240a5a4a02aa04e01e4e97
Guido Diego Quispe Robles <quispeguido2002@gmail.com>
2023-12-27 23:46:33 -0300

    Update plants
    functions for creation and deletion plants

--- inc/plants.hpp
+++ inc/plants.hpp
@@ -3,20 +3,28 @@
 #pragma once
 #include <tyra>
 #include <vector>
-#include <string>
 
 using Tyra::Sprite;
 
-enum Plant_State_enum {None,PeaShotter};
+extern Tyra::Texture* projectilePea;
+extern int plantsCreated;
+
+enum Plant_State_enum {NonePlant,PeaShotter};
 class Plant{
     
     void createSpace();
     public:
-    Plant_State_enum type = None;
+    Plant_State_enum type = NonePlant;
     
     std::vector <int> id;
 
     int* father;
     std::vector <int*> body;
     void newPlant(Plant_State_enum newType);
+    int attackTimer = 0;
+    int row;
+    int column;
 };
+
+void deletePeashotter(const int pos);
+void createPlant(const int row, const int column);
--- src/plants.cpp
+++ src/plants.cpp
@@ -1,26 +1,112 @@
 // File for all plants data
+#include "main.hpp"
 #include "plants.hpp"
+#include "components.hpp"
 
-void Plant::createSpace(){
-    switch (type)
-    {
+int plantsCreated = 0;
+
+void Plant::createSpace() {
+  switch (type) {
     case PeaShotter:
-        id.reserve(3);
-        body.reserve(2);
-        father = &id[0];
-        body[0] = &id[1];
-        body[1] = &id[2];
-        break;
-    
+      id.reserve(3);
+      body.reserve(2);
+      father = &id[0];
+      body[0] = &id[1];
+      body[1] = &id[2];
+      // body[2] = &id[3];
+      break;
+
     default:
-        break;
-    }
+      break;
+  }
 }
 
-void Plant::newPlant(Plant_State_enum newType){
-    type = newType;
-    createSpace();
+void Plant::newPlant(Plant_State_enum newType) {
+  type = newType;
+  createSpace();
 }
-// class PeaShooter{
 
-// };
+void createPeashotter(int id, int row, int column, Tyra::Vec2 pos) {
+  plant[id].newPlant(PeaShotter);
+  
+  plant[id].row = row;
+  plant[id].column = column;
+
+  plant[id].id[0] = Entities::newID();
+  plant[id].id[1] = Entities::newID();
+  plant[id].id[2] = Entities::newID();
+
+  // printf("plant[%d].id[0]: %d\n",pos,plant[pos].id[0]);
+  // printf("plant[%d].id[1]: %d\n",pos,plant[pos].id[1]);
+  // printf("plant[%d].id[2]: %d\n",pos,plant[pos].id[2]);
+
+  vec2Array[*plant[id].father] = pos;  // Vec2(row, column);
+
+  createSprite(*plant[id].body[0], Tyra::MODE_STRETCH, Vec2(pos.x, pos.y + 5),
+               Vec2(128 / 1.6f, 128 / 1.6f));
+  createSprite(*plant[id].body[1], Tyra::MODE_STRETCH, Vec2(pos.x, pos.y + 5),
+               Vec2(128 / 1.6f, 128 / 1.6f));
+
+  animationArray[*plant[id].body[0]] = Animation(peaShooterHead);
+
+  animationDataArray[peaShooterHead].keys[0]->addLink(
+      spriteArray[*plant[id].body[0]].id);
+  animationDataArray[peaShooterBody].keys[0]->addLink(
+      spriteArray[*plant[id].body[1]].id);
+
+  // Life
+
+  lifeArray[*plant[id].body[0]] = 300;
+
+  // HitBox
+  boxColliderArray[*plant[id].body[0]] =
+      BoxCollider(pos.x + 10, pos.y + 20, 28, 38);
+  createDebugBoxCollider(*plant[id].body[0], Tyra::MODE_STRETCH);
+
+  // proyectile
+  pointColliderArray[*plant[id].body[0]] = Vec2(pos.x + 40, pos.y + 25);
+  createDebugPoint(*plant[id].body[0], Tyra::MODE_STRETCH);
+}
+
+void deletePeashotter(const int pos) {
+  plant[pos].type = NonePlant;
+  
+  plantCreatedInMap[plant[pos].row][plant[pos].column] = false;
+
+  vec2Array.erase(*plant[pos].father);
+
+  engine->renderer.getTextureRepository()
+      .getBySpriteId(spriteArray[*plant[pos].body[0]].id)
+      ->removeLinkById(spriteArray[*plant[pos].body[0]].id);
+
+  engine->renderer.getTextureRepository()
+      .getBySpriteId(spriteArray[*plant[pos].body[1]].id)
+      ->removeLinkById(spriteArray[*plant[pos].body[1]].id);
+
+  animationArray.erase(*plant[pos].body[0]);
+
+  deleteSprite(*plant[pos].body[0]);
+  deleteSprite(*plant[pos].body[1]);
+
+  lifeArray.erase(*plant[pos].body[0]);
+
+  deleteDebugBoxCollider(*plant[pos].body[0]);
+  deleteDebugPoint(*plant[pos].body[0]);
+  Entities::deleteID(*plant[pos].father);
+  Entities::deleteID(*plant[pos].body[0]);
+  Entities::deleteID(*plant[pos].body[1]);
+  plantsCreated--;
+}
+
+void createPlant(const int row, const int column) {
+  if (plantCreatedInMap[row][column] == false) {
+    plantCreatedInMap[row][column] = true;
+    createPeashotter(plantsCreated,row,column, Vec2(mapCollider[row][column].x,
+                                         mapCollider[row][column].y));
+    plantsCreated++;
+    // printf("estoy en tile %f,%f\n",cursorTile.x,cursorTile.y);
+    // printf("plantas creadas: %d\n",plantsCreated);
+  } else {
+    printf("no se puede crear aqui, ya existe una planta\n");
+  }
+}
