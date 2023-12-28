f035423e5da73acdf18630785f31fddeae57752e
Guido Diego Quispe Robles <quispeguido2002@gmail.com>
2023-12-27 23:50:50 -0300

    Update Debug
    new creation and deletion functions

--- inc/debug.hpp
+++ inc/debug.hpp
@@ -1,14 +1,29 @@
 #pragma once
+#include <tyra>
 #include <stdio.h>
+#include "renderSprite/textures.hpp"
 
 enum debugModes { AnimationDebug, testDebug, debugModesSize};
 
 extern unsigned int debugOption;
 
 extern bool debugMode ;
-extern bool debugAnimation ;
+extern bool debugAnimation;
 extern bool stopAnimation;
+
+extern std::map <int,Tyra::Sprite> debugSpritePointCollider;
+extern std::map <int,Tyra::Sprite> debugSpriteBoxCollider;
+
+extern Tyra::Texture* debugBoxTexture;
+extern Tyra::Texture* debugPointTexture;
 
 void debugMenu();
 void activeDebugMode();
 void deactiveDebugMode();
+void loadDebugTextures();
+
+void createDebugBoxCollider(const int id, Tyra::SpriteMode mode);
+void createDebugPoint(const int id, Tyra::SpriteMode mode);
+
+void deleteDebugBoxCollider(const int id);
+void deleteDebugPoint(const int id);
--- src/debug.cpp
+++ src/debug.cpp
@@ -1,9 +1,13 @@
 #include "debug.hpp"
+#include "components.hpp"
 
 unsigned int debugOption = 0;
 bool debugMode = false;
 bool debugAnimation = false;
 bool stopAnimation = false;
+
+Tyra::Texture* debugBoxTexture;
+Tyra::Texture* debugPointTexture;
 
 void debugMenu(){
     printf("PRESS X FOR ACTIVE\n");
@@ -54,4 +58,34 @@ void deactiveDebugMode(){
     default:
         break;
     }
+}
+
+void loadDebugTextures(){
+    debugBoxTexture = loadTexture("asset_box.png");
+    debugPointTexture = loadTexture("debugPoint.png");
+}
+
+void createDebugBoxCollider(const int id, Tyra::SpriteMode mode){
+    // printf("id debug box: %d\n", id);
+    debugSpriteBoxCollider[id] = Sprite();
+    loadSprite(&debugSpriteBoxCollider[id],mode,Vec2(boxColliderArray[id].x,boxColliderArray[id].y),Vec2(boxColliderArray[id].width,boxColliderArray[id].height));
+    debugBoxTexture->addLink(debugSpriteBoxCollider[id].id);
+}
+
+void deleteDebugBoxCollider(const int id){
+    debugBoxTexture->removeLinkById(debugSpriteBoxCollider[id].id);
+    debugSpriteBoxCollider.erase(id);
+}
+
+void createDebugPoint(const int id, Tyra::SpriteMode mode){
+    // printf("id point box: %d\n", id);
+    debugSpritePointCollider[id] = Sprite();
+    loadSprite(&debugSpritePointCollider[id],mode,Vec2(pointColliderArray[id].x-1,pointColliderArray[id].y-1),Vec2(31,31));
+    debugPointTexture->addLink(debugSpritePointCollider[id].id);
+    debugSpritePointCollider[id].color = Tyra::Color(255.0f,0.0f,0.0f);
+}
+
+void deleteDebugPoint(const int id){
+    debugPointTexture->removeLinkById(debugSpritePointCollider[id].id);
+    debugSpritePointCollider.erase(id);
 }
